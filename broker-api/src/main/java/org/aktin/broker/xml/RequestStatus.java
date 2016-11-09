9e53b41a9d05f8c48bf717ee8ddcc3b68dd4901d
R.W.Majeed <raphael@majeed.de>
2016-11-09 23:50:12 +0100

    javadoc

--- broker-api/src/main/java/org/aktin/broker/xml/RequestStatus.java
+++ broker-api/src/main/java/org/aktin/broker/xml/RequestStatus.java
@@ -1,5 +1,17 @@
 package org.aktin.broker.xml;
 
 public enum RequestStatus {
-	retrieved, accepted, processing, completed, rejected, failed; 
+	// TODO refactor to uppercase constant names
+	/** The request has been retrieved by the node. */
+	retrieved,
+	/** The request was accepted for processing, but is currently waiting in the execution queue. XXX rename to queued */
+	accepted,
+	/** The request is currently being processed.  */
+	processing,
+	/** The request completed successfully and results have been submitted */
+	completed, 
+	/** The request was rejected. */
+	rejected,
+	/** An error occurred during request processing at the node */
+	failed; 
 }
