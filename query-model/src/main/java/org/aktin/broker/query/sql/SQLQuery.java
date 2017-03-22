d39780594dd19e89e4979463f83aab31ed7e6dc2
R.W.Majeed <raphael@majeed.de>
2017-03-22 21:38:00 +0100

    use separate XML namespace for sql queries

--- query-model/src/main/java/org/aktin/broker/query/sql/SQLQuery.java
+++ query-model/src/main/java/org/aktin/broker/query/sql/SQLQuery.java
@@ -3,7 +3,9 @@ package org.aktin.broker.query.sql;
 import java.util.List;
 
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement(name="sql")
 public class SQLQuery {
 	@XmlElement
 	List<Source> source;
