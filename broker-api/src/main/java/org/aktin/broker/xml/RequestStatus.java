df7f7f75a34ce6a76a7ab5ec42a7e151296a7dde
R.W.Majeed <raphael@majeed.de>
2017-04-03 11:55:00 +0100

    status added for user interaction

--- broker-api/src/main/java/org/aktin/broker/xml/RequestStatus.java
+++ broker-api/src/main/java/org/aktin/broker/xml/RequestStatus.java
@@ -4,13 +4,17 @@ public enum RequestStatus {
 	// TODO refactor to uppercase constant names
 	/** The request has been retrieved by the node. */
 	retrieved,
+	/** The request is waiting for user interaction,
+	 * e.g. manual confirmation of execution o result submission.
+	 */
+	interaction,
 	/** The request was accepted for processing, but is currently waiting in the execution queue. XXX rename to queued */
 	queued,
 	/** The request is currently being processed.  */
 	processing,
 	/** The request completed successfully and results have been submitted */
 	completed, 
-	/** The request was rejected and will not processed */
+	/** The request was rejected and will not be processed */
 	rejected,
 	/** An error occurred during request processing at the node */
 	failed; 
