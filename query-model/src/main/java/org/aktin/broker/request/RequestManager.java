64f39251322a403a0f6218f46867769e52580734
R.W.Majeed <raphael@majeed.de>
2017-04-06 01:07:59 +0200

    CDI event processing added

--- query-model/src/main/java/org/aktin/broker/request/RequestManager.java
+++ query-model/src/main/java/org/aktin/broker/request/RequestManager.java
@@ -1,15 +1,31 @@
 package org.aktin.broker.request;
 
+import java.util.List;
+
+/**
+ * Request manager interface for a single broker.
+ *
+ * @author R.W.Majeed
+ *
+ */
 public interface RequestManager {
 
-	Iterable<RetrievedRequest> getRequests(boolean hidden);
-	RetrievedRequest getRequest(int id);
+	List<? extends RetrievedRequest> getRequests();
+
+	default RetrievedRequest getRequest(int id){
+		for( RetrievedRequest r : getRequests() ){
+			if( r.getRequestId() == id ){
+				return r;
+			}
+		}
+		return null;
+	}
 	/**
 	 * Get requests associated with the specified query.
 	 * All returned requests belong to the same query id.
 	 * @param queryId query id
 	 * @return requests belonging to the specified query id
 	 */
-	Iterable<RetrievedRequest> getQueryRequests(int queryId);
+	List<? extends RetrievedRequest> getQueryRequests(int queryId);
 
 }
