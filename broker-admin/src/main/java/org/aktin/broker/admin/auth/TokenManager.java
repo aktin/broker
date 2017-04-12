3fc1f8a9554cba65eb8adf0701c2f72e988650ef
R.W.Majeed <raphael@majeed.de>
2017-04-12 22:12:31 +0200

    fixed property name. if unspecified, generate password

--- broker-admin/src/main/java/org/aktin/broker/admin/auth/TokenManager.java
+++ broker-admin/src/main/java/org/aktin/broker/admin/auth/TokenManager.java
@@ -2,15 +2,30 @@ package org.aktin.broker.admin.auth;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
 import javax.inject.Singleton;
 
 @Singleton
 public class TokenManager {
+	private static final Logger log = Logger.getLogger(TokenManager.class.getName());
 	private Map<String,Token> map;
+	private String simplePassword;
 
 	public TokenManager(){
 		this.map = new HashMap<>();
+		simplePassword = System.getProperty("aktin.broker.password");
+		if( simplePassword == null ){
+			// generate random password
+			StringBuilder b = new StringBuilder(8);
+			for( int i=0; i<b.capacity(); i++ ){
+				b.append(Character.valueOf((char)('0'+Math.random()*('Z'-'0'))));
+			}
+			simplePassword = b.toString();
+		}
+		// TODO use normal logging or even better real password management
+		System.err.println("Using password: "+simplePassword);
+		log.info("Using password: "+simplePassword);
 	}
 	/**
 	 * Authenticate user and register token
@@ -20,8 +35,7 @@ public class TokenManager {
 	 */
 	Token authenticate(String username, char[] charArray){
 		// XXX for rapid prototyping, use system property for password
-		String pw = System.getProperty("áktin.broker.password","");
-		if( !pw.equals(new String(charArray)) ){
+		if( !simplePassword.equals(new String(charArray)) ){
 			// invalid password, fail authentication by returning null token
 			return null;
 		}
