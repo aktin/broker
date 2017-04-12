bbbb9cda02f19dc323876f6c8f7ee648fb114d43
R.W.Majeed <raphael@majeed.de>
2017-04-12 21:41:15 +0200

    check password against system property

--- broker-admin/src/main/java/org/aktin/broker/admin/auth/TokenManager.java
+++ broker-admin/src/main/java/org/aktin/broker/admin/auth/TokenManager.java
@@ -3,6 +3,9 @@ package org.aktin.broker.admin.auth;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.inject.Singleton;
+
+@Singleton
 public class TokenManager {
 	private Map<String,Token> map;
 
@@ -16,6 +19,12 @@ public class TokenManager {
 	 * @return token
 	 */
 	Token authenticate(String username, char[] charArray){
+		// XXX for rapid prototyping, use system property for password
+		String pw = System.getProperty("áktin.broker.password","");
+		if( !pw.equals(new String(charArray)) ){
+			// invalid password, fail authentication by returning null token
+			return null;
+		}
 		Token t = new Token(username);
 		map.put(t.getGUID(), t);
 		return t;
