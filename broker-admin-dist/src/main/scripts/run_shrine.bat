a503f316d37c4c104e9c59c11452bc83469f242b
R.W. Majeed <rwmajeed@gmail.com>
2019-11-04 19:47:45 +0100

    add logging configuration

--- broker-admin-dist/src/main/scripts/run_shrine.bat
+++ broker-admin-dist/src/main/scripts/run_shrine.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 
-java -Daktin.broker.password=CHANGEME -cp lib\* org.aktin.broker.admin.standalone.HttpServer 8080
+java -Daktin.broker.password=CHANGEME -Djava.util.logging.config.file=logging.properties -cp lib\* org.aktin.broker.admin.standalone.HttpServer 8080
--- broker-admin-dist/src/main/scripts/run_shrine.sh
+++ broker-admin-dist/src/main/scripts/run_shrine.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -Daktin.broker.password=CHANGEME -cp lib/\* org.aktin.broker.admin.standalone.HttpServer 8080
+java -Daktin.broker.password=CHANGEME -Djava.util.logging.config.file=logging.properties -Dorg.eclipse.jetty.util.log.class=org.eclipse.jetty.util.log.JavaUtilLog -cp lib/\* org.aktin.broker.admin.standalone.HttpServer 8080
