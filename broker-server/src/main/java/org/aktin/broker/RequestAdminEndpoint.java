f20d5c3b6ee76032368661fedf5d4dc77aea9960
R.W. Majeed <rwmajeed@gmail.com>
2020-12-12 02:54:01 +0100

    remove unused import

--- broker-server/src/main/java/org/aktin/broker/RequestAdminEndpoint.java
+++ broker-server/src/main/java/org/aktin/broker/RequestAdminEndpoint.java
@@ -24,7 +24,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.Response.Status;
