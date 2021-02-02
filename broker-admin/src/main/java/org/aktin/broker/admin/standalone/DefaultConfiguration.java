d1c27ac3eadbd9a3cd80041164225b0729e8b254
R.W. Majeed <rwmajeed@gmail.com>
2021-02-02 17:56:10 +0100

    update default configuration to use system property for auth provider selection

--- broker-admin/src/main/java/org/aktin/broker/admin/standalone/DefaultConfiguration.java
+++ broker-admin/src/main/java/org/aktin/broker/admin/standalone/DefaultConfiguration.java
@@ -1,14 +1,43 @@
 package org.aktin.broker.admin.standalone;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
+import org.aktin.broker.auth.CascadedAuthProvider;
 import org.aktin.broker.server.auth.AuthProvider;
 
 public class DefaultConfiguration implements Configuration{
+	private static final String DEFAULT_AUTH_PROVIDER = "org.aktin.broker.auth.apikey.ApiKeyPropertiesAuthProvider,org.aktin.broker.auth.cred.CredentialTokenAuthProvider";
+	private AuthProvider authProvider;
 
+	
 	public DefaultConfiguration() {
-		
+		String ap = System.getProperty("broker.auth.provider", DEFAULT_AUTH_PROVIDER);
+
+		String[] aps = ap.split(",");
+		if( aps.length == 1 ) {
+			// single auth provider
+			authProvider = loadProviderClass(aps[0]);
+		}else {
+			// multiple auth providers
+			AuthProvider[] provs = new AuthProvider[aps.length];
+			for( int i=0; i<provs.length; i++ ) {
+				provs[i] = loadProviderClass(aps[i].trim());
+			}
+			authProvider = new CascadedAuthProvider(Arrays.asList(provs));
+		}
+	}
+	private AuthProvider loadProviderClass(String name) throws IllegalArgumentException {
+		Constructor<?> c;
+		try {
+			c = Class.forName(name).getConstructor();
+			return (AuthProvider)c.newInstance();
+		} catch (NoSuchMethodException | SecurityException | ClassNotFoundException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+			throw new IllegalArgumentException("Unable to instantiate auth provider class: "+name, e);
+		}
 	}
 
 	@Override
@@ -36,8 +65,7 @@ public class DefaultConfiguration implements Configuration{
 
 	@Override
 	public AuthProvider getAuthProvider() {
-		// TODO Auto-generated method stub
-		return null;
+		return authProvider;
 	}
 
 	@Override
