f75090821a68265af8fff474a3ecfb64a683880f
R.W. Majeed <rwmajeed@gmail.com>
2021-02-02 17:20:55 +0100

    allow direct initialization with property file input stream

--- broker-auth-local/src/main/java/org/aktin/broker/auth/apikey/ApiKeyPropertiesAuthProvider.java
+++ broker-auth-local/src/main/java/org/aktin/broker/auth/apikey/ApiKeyPropertiesAuthProvider.java
@@ -7,20 +7,31 @@ import java.nio.file.Files;
 import org.aktin.broker.server.auth.AbstractAuthProviderFactory;
 
 public class ApiKeyPropertiesAuthProvider extends AbstractAuthProviderFactory {
+	private PropertyFileAPIKeys keys;
+
+	public ApiKeyPropertiesAuthProvider() {
+		keys = null; // lazy init, load later in getInstance by using supplied path
+	}
+	public ApiKeyPropertiesAuthProvider(InputStream in) throws IOException {
+		this.keys = new PropertyFileAPIKeys(in);
+	}
+
 
 	@Override
 	public PropertyFileAPIKeys getInstance() throws IOException {
-
-		PropertyFileAPIKeys auth;
-		try( InputStream in = Files.newInputStream(path.resolve("api-keys.properties")) ){
-			auth = new PropertyFileAPIKeys(in);
+		if( this.keys == null ) {
+			// not previously loaded
+			try( InputStream in = Files.newInputStream(path.resolve("api-keys.properties")) ){
+				this.keys = new PropertyFileAPIKeys(in);
+			}
+		}else {
+			;// already loaded, use existing one
 		}
+		return keys;
 //		if( System.getProperty("rewriteNodeDN") != null ){
 //			int count = BrokerImpl.updatePrincipalDN(ds, keys.getMap());
 //			// output/log what happened, use count returned from above method
 //			System.out.println("Rewritten "+count+" node DN strings.");
 //		}
-
-		return auth;
 	}
 }
