296f1c5e323a079b71e1725ae27df9c0bcd03ea7
R.W. Majeed <rwmajeed@gmail.com>
2021-03-09 00:45:51 +0100

    add websocket closed notification to admin listener

--- broker-client/src/main/java/org/aktin/broker/client2/AdminNotificationListener.java
+++ broker-client/src/main/java/org/aktin/broker/client2/AdminNotificationListener.java
@@ -7,4 +7,5 @@ public interface AdminNotificationListener {
 	void onRequestStatusUpdate(int requestId, int nodeId, String status);
 	void onRequestResultUpdate(int requestId, int nodeId, String mediaType);
 	void onResourceUpdate(int nodeId, String resourceId);
+	void onWebsocketClosed(int statusCode, String reason);
 }
