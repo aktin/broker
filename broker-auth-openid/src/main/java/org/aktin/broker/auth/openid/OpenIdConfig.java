0e5a594e989a85eaa086d0c16e402161f292fd64
Michael Folz <folz@med.uni-frankfurt.de>
2021-03-09 15:53:41 +0100

    Offline JWT validation
    - remove online token introspection
    - verify tokens locally

--- broker-auth-openid/src/main/java/org/aktin/broker/auth/openid/OpenIdConfig.java
+++ broker-auth-openid/src/main/java/org/aktin/broker/auth/openid/OpenIdConfig.java
@@ -2,27 +2,35 @@ package org.aktin.broker.auth.openid;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
+import java.util.stream.Collectors;
 import lombok.Data;
 
 @Data
 public class OpenIdConfig {
 
+  private String jwks_uri;
   private String auth_host;
-  private String clientId;
-  private String clientSecret;
   private String siteNameClaim;
+  private List<String> allowedAlgorithms;
 
   public OpenIdConfig(InputStream in) {
     Properties prop = new Properties();
     try {
       prop.load(in);
+      setJwks_uri(prop.getProperty("openid.jwks_uri"));
       setAuth_host(prop.getProperty("openid.server"));
-      setClientId(prop.getProperty("openid.client.id"));
-      setClientSecret(prop.getProperty("openid.client.secret"));
       setSiteNameClaim(prop.getProperty("openid.claim.site-name"));
+      setAllowedAlgorithms(extractAlgorithmsList(prop.getProperty("openid.algorithms")));
     } catch (IOException e) {
       e.printStackTrace();
     }
+  }
+
+  private List<String> extractAlgorithmsList(String algorithmsString) {
+    List<String> algorithmsList = Arrays.asList(algorithmsString.split(","));
+    return algorithmsList.stream().map(String::trim).collect(Collectors.toList());
   }
 }
