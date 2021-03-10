8541cb7408a98f513a1660f762d1005781079ee6
Michael Folz <folz@med.uni-frankfurt.de>
2021-03-10 15:28:18 +0100

    Offline JWT validation
    - provide filename for keycloak.json to AuthzClient

--- broker-client/src/main/java/org/aktin/broker/client2/auth/OpenIdAuthentication.java
+++ broker-client/src/main/java/org/aktin/broker/client2/auth/OpenIdAuthentication.java
@@ -1,18 +1,27 @@
 package org.aktin.broker.client2.auth;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import org.keycloak.authorization.client.AuthzClient;
 
-public class OpenIdAuthentication extends HttpAuthorizationBearerAuth{
-	private String apiKey;
+public class OpenIdAuthentication extends HttpAuthorizationBearerAuth {
 
-	public OpenIdAuthentication(String apiKey) {
-		this.apiKey = apiKey;
-	}
+  private String apiKey;
 
-	@Override
-	protected String getBearerToken() throws IOException {
-		return AuthzClient.create().obtainAccessToken().getToken();
-	}
+  public OpenIdAuthentication(String apiKey) {
+    this.apiKey = apiKey;
+  }
+
+  @Override
+  protected String getBearerToken() throws IOException {
+    try (InputStream in = Files.newInputStream(Paths.get("keycloak.json"))) {
+      return AuthzClient.create(in).obtainAccessToken().getToken();
+    } catch (IOException e) {
+			System.err.println("Unable to read keycloak.json: " + e.getMessage());
+			throw(e);
+		}
+  }
 
 }
