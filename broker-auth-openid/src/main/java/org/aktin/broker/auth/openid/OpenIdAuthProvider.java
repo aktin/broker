10faa9fa0fb2f3b5108b3a1305d6a49e8af485aa
R.W. Majeed <rwmajeed@gmail.com>
2021-03-11 21:34:38 +0100

    always return same instance of authenticator

--- broker-auth-openid/src/main/java/org/aktin/broker/auth/openid/OpenIdAuthProvider.java
+++ broker-auth-openid/src/main/java/org/aktin/broker/auth/openid/OpenIdAuthProvider.java
@@ -7,9 +7,10 @@ import lombok.Setter;
 import org.aktin.broker.server.auth.AbstractAuthProvider;
 import org.aktin.broker.server.auth.HeaderAuthentication;
 
-@Setter
 public class OpenIdAuthProvider extends AbstractAuthProvider{
+	@Setter
 	private OpenIdConfig config;
+	private OpenIdAuthenticator auth;
 
 	public OpenIdAuthProvider() {
 		config = null; // lazy init, load later in getInstance by using supplied path
@@ -18,16 +19,21 @@ public class OpenIdAuthProvider extends AbstractAuthProvider{
 	public OpenIdAuthProvider(InputStream in) {
 		this.config = new OpenIdConfig(in);
 	}
-	public void loadConfig() throws IOException {
-		// TODO initialize configuration from config file in this.path
+	private void loadConfig() throws IOException {
+		if( this.config != null ) {
+			// don't load if configuration already available, e.g. via setter method
+			return;
+		}
+		// initialize configuration from config file in this.path
 		try (InputStream in = Files.newInputStream(path.resolve("openid-config.properties"))) {
 			this.config = new OpenIdConfig(in);
 		}
 	}
 	@Override
 	public HeaderAuthentication getInstance() throws IOException {
-		if( this.config == null ) {
+		if( this.auth == null ) {
 			loadConfig();
+			this.auth = new OpenIdAuthenticator(this.config);
 		}
 		return new OpenIdAuthenticator(config);
 	}
