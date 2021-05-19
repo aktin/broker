4c04cab056fcef893eaaf38aca000374ee0f0c74
Julian Gründner <juliangruendner@googlemail.com>
2021-05-19 07:31:56 +0200

    change dockrfiles to unprivileged users and expose env vars aktin-client

--- docker/aktin-broker/run_broker.sh
+++ docker/aktin-broker/run_broker.sh
@@ -4,4 +4,4 @@
 HOST_AND_PORT=${HOST_AND_PORT:-"0.0.0.0:8080"}
 PASSWORD=${PASSWORD:-"CHANGEME"}
 
-"$JAVA_HOME"/bin/java -Daktin.broker.password=$PASSWORD -Djava.util.logging.config.file=logging.properties -Dorg.eclipse.jetty.util.log.class=org.eclipse.jetty.util.log.JavaUtilLog -cp lib/\* org.aktin.broker.admin.standalone.HttpServer $HOST_AND_PORT
+"$JAVA_HOME"/bin/java -Daktin.broker.password=$PASSWORD -Djava.util.logging.config.file=logging.properties -Dorg.eclipse.jetty.util.log.class=org.eclipse.jetty.util.log.JavaUtilLog -cp lib/\* org.aktin.broker.admin.standalone.HttpServer $HOST_AND_PORT 2>&1
