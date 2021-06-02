370c3b04cce4a91dbe7ef607254ec99c42b9f95c
R.W. Majeed <rwmajeed@gmail.com>
2021-06-02 13:55:57 +0200

    added comments to broker startup script

--- broker-admin-dist/src/main/scripts/run_broker.sh
+++ broker-admin-dist/src/main/scripts/run_broker.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 "$JAVA_HOME"/bin/java -Daktin.broker.password=CHANGEME -Djava.util.logging.config.file=logging.properties -cp lib/\* org.aktin.broker.admin.standalone.HttpServer 8080
+
+# You can override the JDBC url by specifying -Daktin.broker.jdbc.url=jdbc:hsqldb:file:./broker\;shutdown=false\;user=admin\;password=secret
+
+# To use a different database e.g. postgres, copy its JDBC driver to the lib folder and use the system properties aktin.broker.jdbc.url and -Daktin.broker.jdbc.datasource.class
