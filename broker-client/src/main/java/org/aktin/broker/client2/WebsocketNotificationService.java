6b6f884b09ae862c38f1684de0c32c8409bd2bb0
R.W. Majeed <rwmajeed@gmail.com>
2021-06-25 02:34:37 +0200

    changed websocket listener to handle incomplete messages. previously stopped working when an incomplete message was encountered

--- broker-client/src/main/java/org/aktin/broker/client2/WebsocketNotificationService.java
+++ broker-client/src/main/java/org/aktin/broker/client2/WebsocketNotificationService.java
@@ -1,31 +1,49 @@
 package org.aktin.broker.client2;
 
-import java.io.IOException;
 import java.net.http.WebSocket;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutorService;
 
+/**
+ * Websocket notification service. Ensures that text messages are complete before 
+ * calling the methods {@link #notifyClose(int)} and {@link #notifyText(String)} asynchronously
+ * with the executor specified in the constructor.
+ * 
+ * @author R.W.Majeed
+ *
+ */
 public abstract class WebsocketNotificationService implements WebSocket.Listener{
 
 	private ExecutorService executor;
+	private StringBuilder text; // collect incomplete parts
 
 	public WebsocketNotificationService(ExecutorService executor) {
 		this.executor = executor;
+		this.text = new StringBuilder();
 	}
 
 	@Override
 	public CompletionStage<?> onText(WebSocket webSocket, CharSequence data, boolean last) {
+		text.append(data);
 		if( last == true ) {
-			webSocket.request(1);
-			return CompletableFuture.runAsync( () -> notifyText(data.toString()), executor);			
+			// process completed message
+			String message = text.toString();
+			text = new StringBuilder();
+			executor.execute( () -> notifyText(message) );
 		}else {
-			return CompletableFuture.failedStage(new IOException("Partial websocket message not supported"));
+			// at a certain load, java will provide incomplete messages
+			;// we don't need to do anything here
 		}
+		webSocket.request(1);
+		return null; // reclaim CharSequence immediately
 	}
 	
 	protected abstract void notifyClose(int statusCode);
 
+	/**
+	 * Handle notification of a completed text message.
+	 * @param text message contents
+	 */
 	protected abstract void notifyText(String text);
 	
 	@Override
@@ -34,6 +52,9 @@ public abstract class WebsocketNotificationService implements WebSocket.Listener
 		return null; // websocket can be closed immediately
 	}
 
+	/**
+	 * Shuts down the executor
+	 */
 	public void shutdown() {
 		executor.shutdownNow();
 	}
