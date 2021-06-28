4d4aac31307bdb131b8404f6f38c3cb44d183b26
Julian Gründner <juliangruendner@googlemail.com>
2021-06-28 11:31:15 +0200

    add java opts to broker client

--- docker/aktin-client/run_client.sh
+++ docker/aktin-client/run_client.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Running aktin client from console"
-java -cp lib/\* org.aktin.broker.client.live.sysproc.CLI sysproc.properties
+java $JAVA_OPTS -cp lib/\* org.aktin.broker.client.live.sysproc.CLI sysproc.properties
