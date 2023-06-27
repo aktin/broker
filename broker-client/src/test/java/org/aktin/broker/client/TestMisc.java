7cb9b17bff193912bdcb68bc0200e51a348719b6
R.W.Majeed <rwmajeed@gmail.com>
2023-06-27 17:45:52 +0200

    add unused test function for local testing of unexpected http behaviour

--- broker-client/src/test/java/org/aktin/broker/client/TestMisc.java
+++ broker-client/src/test/java/org/aktin/broker/client/TestMisc.java
@@ -1,7 +1,13 @@
 package org.aktin.broker.client;
 
+import java.io.IOException;
+import java.net.URI;
+import java.util.List;
+
 import javax.net.ssl.KeyManagerFactory;
 
+import org.aktin.broker.client2.BrokerClient2;
+import org.aktin.broker.xml.RequestInfo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +20,17 @@ public class TestMisc {
 		String defaultAlgo = KeyManagerFactory.getDefaultAlgorithm();
 		Assertions.assertNotNull(defaultAlgo);
 		System.out.println("KeyManagerFactory.defaultAlgo: "+defaultAlgo);
+	}
+
+	//enable for local testing unexpected http behaviour
+	//@Test
+	public void expectExceptionForNonXmlResponse() throws IOException {
+		BrokerClient2 c = new BrokerClient2(URI.create("http://localhost:8080/bla"));
+
+		List<RequestInfo>r = c.listMyRequests();
+
+		System.out.println(r);
+
 	}
 
 }
