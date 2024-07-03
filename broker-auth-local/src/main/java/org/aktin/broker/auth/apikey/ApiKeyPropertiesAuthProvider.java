0de5f119eb87e4d28fc075361896f219c803722b
Alexander Kombeiz <alexkombie@gmail.com>
2024-07-03 14:14:32 +0200

    refactored PropertyFileAPIKeys and ApiKeyPropertiesAuthProvider slightly

--- broker-auth-local/src/main/java/org/aktin/broker/auth/apikey/ApiKeyPropertiesAuthProvider.java
+++ broker-auth-local/src/main/java/org/aktin/broker/auth/apikey/ApiKeyPropertiesAuthProvider.java
@@ -3,30 +3,32 @@ package org.aktin.broker.auth.apikey;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
-
+import java.nio.file.Path;
 import org.aktin.broker.server.auth.AbstractAuthProvider;
 
 public class ApiKeyPropertiesAuthProvider extends AbstractAuthProvider {
-	private PropertyFileAPIKeys keys;
 
-	public ApiKeyPropertiesAuthProvider() {
-		keys = null; // lazy init, load later in getInstance by using supplied path
-	}
-	public ApiKeyPropertiesAuthProvider(InputStream in) throws IOException {
-		this.keys = new PropertyFileAPIKeys(in);
-	}
+  private PropertyFileAPIKeys keys;
 
+  public ApiKeyPropertiesAuthProvider() {
+    keys = null; // lazy init, load later in getInstance by using supplied path
+  }
 
-	@Override
-	public PropertyFileAPIKeys getInstance() throws IOException {
-		if( this.keys == null ) {
-			// not previously loaded
-			try( InputStream in = Files.newInputStream(path.resolve("api-keys.properties")) ){
-				this.keys = new PropertyFileAPIKeys(in);
-			}
-		}else {
-			;// already loaded, use existing one
-		}
-		return keys;
-	}
+  public ApiKeyPropertiesAuthProvider(InputStream in) throws IOException {
+    this.keys = new PropertyFileAPIKeys(in);
+  }
+
+  @Override
+  public PropertyFileAPIKeys getInstance() throws IOException {
+    if (this.keys == null) {
+      try (InputStream in = Files.newInputStream(getPropertiesPath())) {
+        this.keys = new PropertyFileAPIKeys(in);
+      }
+    }
+    return keys;
+  }
+
+  private Path getPropertiesPath() {
+    return path.resolve("api-keys.properties");
+  }
 }
--- broker-auth-local/src/main/java/org/aktin/broker/auth/apikey/PropertyFileAPIKeys.java
+++ broker-auth-local/src/main/java/org/aktin/broker/auth/apikey/PropertyFileAPIKeys.java
@@ -4,29 +4,32 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 import java.util.logging.Logger;
-
 import org.aktin.broker.server.auth.AuthInfo;
 import org.aktin.broker.server.auth.AuthInfoImpl;
 import org.aktin.broker.server.auth.HttpBearerAuthentication;
 
 public class PropertyFileAPIKeys extends HttpBearerAuthentication {
-	private static final Logger log = Logger.getLogger(PropertyFileAPIKeys.class.getName());
 
-	private Properties properties;
+  private static final Logger log = Logger.getLogger(PropertyFileAPIKeys.class.getName());
+  private final Properties properties;
 
-	public PropertyFileAPIKeys(InputStream in) throws IOException {
-		properties = new Properties();
-		properties.load(in);
-		log.info("Loaded "+properties.size()+" client API keys");
-	}
+  public PropertyFileAPIKeys(InputStream in) throws IOException {
+    this.properties = new Properties();
+    loadProperties(in);
+  }
 
-	@Override
-	protected AuthInfo lookupAuthInfo(String token) throws IOException {
-		String clientDn = properties.getProperty(token);
-		if( clientDn == null ) {
-			// unauthorized
-			return null;
-		}
-		return new AuthInfoImpl(token, clientDn, HttpBearerAuthentication.defaultRolesForClientDN(clientDn));
-	}
+  public void loadProperties(InputStream in) throws IOException {
+    properties.load(in);
+    log.info("Loaded " + properties.size() + " client API keys");
+  }
+
+  @Override
+  protected AuthInfo lookupAuthInfo(String token) {
+    String clientDn = properties.getProperty(token);
+    return clientDn != null ? createAuthInfo(token, clientDn) : null;
+  }
+
+  private AuthInfo createAuthInfo(String token, String clientDn) {
+    return new AuthInfoImpl(token, clientDn, HttpBearerAuthentication.defaultRolesForClientDN(clientDn));
+  }
 }
