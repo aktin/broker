9e0f6284428723bdfc6163acc222a7d160243a89
Alexander Kombeiz <alexkombie@gmail.com>
2024-07-05 13:03:02 +0200

    Revert "adjusted test api-keys.properties for new active/inactive state of api keys, added junit test for inactive api key in TestPropertyFileApiKeys"
    This reverts commit cbed9c0938a861a2111fcdaa423768d0d8727b4d.

--- broker-auth-local/src/test/java/org/aktin/broker/auth/apikey/TestPropertyFileApiKeys.java
+++ broker-auth-local/src/test/java/org/aktin/broker/auth/apikey/TestPropertyFileApiKeys.java
@@ -14,7 +14,6 @@ import org.junit.Test;
 public class TestPropertyFileApiKeys {
 	private static final String FIRST_API_KEY = "xxxApiKey123";
 	private static final String FIRST_CLIENT_DN = "CN=Schwarzwaldklinik,O=Schwarzwaldklinik,L=Schwarzwald";
-	private static final String SECOND_API_KEY = "xxxApiKey567";
 	@Test
 	public void getInstanceFromProvider() throws IOException {
 		ApiKeyPropertiesAuthProvider prov = new ApiKeyPropertiesAuthProvider();
@@ -35,18 +34,10 @@ public class TestPropertyFileApiKeys {
 		// set headers
 		Map<String,String> headers = new HashMap<>();
 		HeaderUtils.putAuthorizationBearerHeader(headers, FIRST_API_KEY);
-		//
+		// 
 		AuthInfo info = ak.authenticateByHeaders(headers::get);
 		Assert.assertNotNull(info);
 		Assert.assertEquals(FIRST_CLIENT_DN, info.getClientDN());
-	}
-	@Test
-	public void inactiveAuthHeaderShouldReturnNullAuth() throws IOException {
-		PropertyFileAPIKeys ak = loadInstance();
-		Map<String,String> headers = new HashMap<>();
-		HeaderUtils.putAuthorizationBearerHeader(headers, SECOND_API_KEY);
-		AuthInfo info = ak.authenticateByHeaders(headers::get);
-		Assert.assertNull(info);
 	}
 	@Test
 	public void missingHeaderShouldReturnNullAuth() throws IOException {
@@ -54,7 +45,7 @@ public class TestPropertyFileApiKeys {
 		// set headers
 		Map<String,String> headers = new HashMap<>();
 		headers.put("Bla", "bla");
-		//
+		// 
 		AuthInfo info = ak.authenticateByHeaders(headers::get);
 		// no auth header should the auth to return null
 		Assert.assertNull(info);
@@ -66,7 +57,7 @@ public class TestPropertyFileApiKeys {
 		Map<String,String> headers = new HashMap<>();
 		// intentionally use wrong token
 		HeaderUtils.putAuthorizationBearerHeader(headers, "x"+FIRST_API_KEY);
-		//
+		// 
 		AuthInfo info = ak.authenticateByHeaders(headers::get);
 		Assert.assertNull(info);
 	}
