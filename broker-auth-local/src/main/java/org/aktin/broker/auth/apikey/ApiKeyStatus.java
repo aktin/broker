8450cf0634617f2dff34d57dcf6b00d01ee2145e
Alexander Kombeiz <alexkombie@gmail.com>
2024-07-05 13:03:18 +0200

    Revert "added java docs to ApiKeyStatus and PropertyFileAPIKeys"
    This reverts commit 11fb942d6c9a95f2a667743e7ee5f2c312ca277e.

--- broker-auth-local/src/main/java/org/aktin/broker/auth/apikey/ApiKeyStatus.java
+++ broker-auth-local/src/main/java/org/aktin/broker/auth/apikey/ApiKeyStatus.java
@@ -4,14 +4,7 @@ package org.aktin.broker.auth.apikey;
  * Enum representing the status of an API key.
  */
 public enum ApiKeyStatus {
-  /**
-   * Represents an active API key. An active key is fully operational and can be used for authentication.
-   */
   ACTIVE,
-
-  /**
-   * Represents an inactive API key. An inactive key is not operational and cannot be used for authentication.
-   */
   INACTIVE;
 
   @Override
@@ -19,13 +12,6 @@ public enum ApiKeyStatus {
     return name().toLowerCase();
   }
 
-  /**
-   * Converts a string to the corresponding ApiKeyStatus enum constant.
-   *
-   * @param value The string representation of the API key status.
-   * @return The corresponding ApiKeyStatus enum constant.
-   * @throws IllegalArgumentException if the input string does not match any defined ApiKeyStatus (case-insensitive).
-   */
   public static ApiKeyStatus fromString(String value) {
     for (ApiKeyStatus status : ApiKeyStatus.values()) {
       if (status.name().equalsIgnoreCase(value)) {
--- broker-auth-local/src/main/java/org/aktin/broker/auth/apikey/PropertyFileAPIKeys.java
+++ broker-auth-local/src/main/java/org/aktin/broker/auth/apikey/PropertyFileAPIKeys.java
@@ -11,52 +11,26 @@ import org.aktin.broker.server.auth.AuthInfo;
 import org.aktin.broker.server.auth.AuthInfoImpl;
 import org.aktin.broker.server.auth.HttpBearerAuthentication;
 
-/**
- * Manages API keys stored in a property file for HTTP Bearer Authentication.
- */
 public class PropertyFileAPIKeys extends HttpBearerAuthentication {
 
   private static final Logger log = Logger.getLogger(PropertyFileAPIKeys.class.getName());
   private final Properties properties;
 
-  /**
-   * Constructs a new PropertyFileAPIKeys instance by loading API keys from an input stream.
-   *
-   * @param in The input stream containing the API key properties.
-   * @throws IOException If an I/O error occurs while reading from the input stream.
-   */
   public PropertyFileAPIKeys(InputStream in) throws IOException {
     this.properties = new Properties();
     properties.load(in);
     log.info("Loaded " + properties.size() + " client API keys");
   }
 
-  /**
-   * @return The Properties object with the API keys.
-   */
   public Properties getProperties() {
     return properties;
   }
 
-  /**
-   * Adds or updates an API key in the properties.
-   *
-   * @param apiKey   The API key to add or update.
-   * @param clientDn The client's Distinguished Name.
-   * @param status   The status of the API key (ACTIVE or INACTIVE).
-   */
   public void putApiKey(String apiKey, String clientDn, ApiKeyStatus status) {
     properties.setProperty(apiKey, clientDn + "," + status.toString());
     log.info("Put API key for client: " + clientDn + " with status: " + status);
   }
 
-  /**
-   * Stores the current API keys to an output stream.
-   *
-   * @param out     The output stream to write the properties to.
-   * @param charset The character set to use for writing.
-   * @throws IOException If an I/O error occurs while writing to the output stream.
-   */
   public void storeProperties(OutputStream out, Charset charset) throws IOException {
     try (OutputStreamWriter writer = new OutputStreamWriter(out, charset)) {
       properties.store(writer, "API Keys");
@@ -64,14 +38,6 @@ public class PropertyFileAPIKeys extends HttpBearerAuthentication {
     log.info("Saved " + properties.size() + " client API keys");
   }
 
-  /**
-   * Looks up authentication information for a given token.
-   * <p>
-   * This method is called during the authentication process to validate API keys. It checks if the token exists and is active.
-   *
-   * @param token The API key token to look up.
-   * @return AuthInfo object if the token is valid and active, null otherwise.
-   */
   @Override
   protected AuthInfo lookupAuthInfo(String token) {
     String property = properties.getProperty(token);
@@ -84,13 +50,6 @@ public class PropertyFileAPIKeys extends HttpBearerAuthentication {
     return null;
   }
 
-  /**
-   * Creates an AuthInfo object for a given token and client DN.
-   *
-   * @param token    The API key token.
-   * @param clientDn The client's Distinguished Name.
-   * @return A new AuthInfo object.
-   */
   private AuthInfo createAuthInfo(String token, String clientDn) {
     return new AuthInfoImpl(token, clientDn, HttpBearerAuthentication.defaultRolesForClientDN(clientDn));
   }
