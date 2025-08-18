3df0bb4582736ee4d2f6116dff0e5360f12d62f6
akomii <alexkombie@gmail.com>
2025-08-18 15:35:01 +0200

    changed TokenManager to an interface, added implementation,

---
+++ broker-auth-local/src/main/java/org/aktin/broker/auth/cred2/auth/TokenManagerImpl.java
@@ -0,0 +1,53 @@
+package org.aktin.broker.auth.cred2.auth;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import javax.inject.Singleton;
+
+@Singleton
+class TokenManagerImpl implements TokenManager {
+
+  private final Map<String, Token> sessions = new ConcurrentHashMap<>();
+
+  public TokenManagerImpl() {
+  }
+
+  @Override
+  public Token issue(String username) {
+    if (username == null) {
+      return null;
+    }
+    Token token = new Token(username);
+    sessions.put(token.getGUID(), token);
+    return token;
+  }
+
+  @Override
+  public Token lookup(String guid) {
+    if (guid == null) {
+      return null;
+    }
+    Token token = sessions.get(guid);
+    if (token == null) {
+      return null;
+    }
+    if (!token.isValid()) {
+      sessions.remove(guid);
+      return null;
+    }
+    return token;
+  }
+
+  @Override
+  public void revoke(String guid) {
+    Token token = sessions.remove(guid);
+    if (token != null) {
+      token.invalidate();
+    }
+  }
+
+  @Override
+  public void pruneInvalid() {
+    sessions.values().removeIf(token -> !token.isValid());
+  }
+}
