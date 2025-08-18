64b9b30c90ef8fafd3b9df72c40a05e80b40da43
akomii <alexkombie@gmail.com>
2025-08-18 16:25:29 +0200

    refactored CredentialTokenAuth to properly check and renew tokens

--- broker-auth-local/src/main/java/org/aktin/broker/auth/cred2/auth/CredentialTokenAuth.java
+++ broker-auth-local/src/main/java/org/aktin/broker/auth/cred2/auth/CredentialTokenAuth.java
@@ -1,33 +1,41 @@
 package org.aktin.broker.auth.cred2.auth;
 
-import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.aktin.broker.server.auth.AuthInfo;
 import org.aktin.broker.server.auth.AuthInfoImpl;
 import org.aktin.broker.server.auth.HttpBearerAuthentication;
 
 public class CredentialTokenAuth extends HttpBearerAuthentication {
 
-  private TokenManager manager;
+  private static final Logger log = Logger.getLogger(CredentialTokenAuth.class.getName());
+
+  private final TokenManager manager;
 
   public CredentialTokenAuth(TokenManager manager) {
     this.manager = manager;
   }
 
+  //TODO all users default to admin role
   private static String getClientDn(Token token) {
     return "CN=" + token.getName() + ",OU=admin";
   }
 
   @Override
-  protected AuthInfo lookupAuthInfo(String token) throws IOException {
-    Token t = manager.lookupToken(token);
-    if (t == null) {
-      // unauthorized
+  protected AuthInfo lookupAuthInfo(String guid) {
+    if (guid == null || guid.isEmpty()) {
       return null;
     }
-    t.renew();
-    String clientDn = getClientDn(t);
-    return new AuthInfoImpl(t.getName(), clientDn, defaultRolesForClientDN(clientDn));
+    Token token = manager.lookup(guid);
+    if (token == null) {
+      return null; // invalid/expired/revoked
+    }
+    try {
+      token.renew();
+    } catch (RuntimeException e) {
+      log.log(Level.WARNING, "Failed to renew token", e);
+    }
+    final String dn = getClientDn(token);
+    return new AuthInfoImpl(token.getName(), dn, defaultRolesForClientDN(dn));
   }
-
-
 }
